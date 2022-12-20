3b70e598f5bbff7298c3e91c36322a0c3acbbdce
pegvin <75035219+pegvin@users.noreply.github.com>
2022-12-20 11:22:42 +0530

    Merge External Library Into Main File & Do Case-Insensitive Comparisons So true == tRuE

--- src/imgui_styles.cpp
+++ src/imgui_styles.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "imgui_styles.h"
-#include "ini.h"
+
+#define INI_VERSION "0.1.1"
+typedef struct ini_t ini_t;
+
+ini_t*      ini_load(const char *filename);
+ini_t*      ini_load_txt(const char *iniTxt);
+void        ini_free(ini_t *ini);
+const char* ini_get(ini_t *ini, const char *section, const char *key);
+int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
+
+/* Case insensitive string compare upto n */
+static int strncmpci(const char* s1, const char* s2, size_t n) {
+    while (n && *s1 && ( tolower(*s1) == tolower(*s2) )) {
+        ++s1;
+        ++s2;
+        --n;
+    }
+    if ( n == 0 ) return 0;
+    else return ( *(unsigned char *)s1 - *(unsigned char *)s2 );
+}
 
 static const char* ImGuiDirToText(int direction) {
 	switch (direction) {
@@ -17,11 +38,11 @@ static const char* ImGuiDirToText(int direction) {
 
 static int ImGuiTextToDir(const char* direction) {
 	if (direction == NULL) return -1;
-	if (strncmp(direction, "None", 4) == 0)       return ImGuiDir_None;
-	else if (strncmp(direction, "Left", 4) == 0)  return ImGuiDir_Left;
-	else if (strncmp(direction, "Right", 5) == 0) return ImGuiDir_Right;
-	else if (strncmp(direction, "Up", 2) == 0)    return ImGuiDir_Up;
-	else if (strncmp(direction, "Down", 4) == 0)  return ImGuiDir_Down;
+	if (strncmpci(direction, "None", 4) == 0)       return ImGuiDir_None;
+	else if (strncmpci(direction, "Left", 4) == 0)  return ImGuiDir_Left;
+	else if (strncmpci(direction, "Right", 5) == 0) return ImGuiDir_Right;
+	else if (strncmpci(direction, "Up", 2) == 0)    return ImGuiDir_Up;
+	else if (strncmpci(direction, "Down", 4) == 0)  return ImGuiDir_Down;
 	else return -1;
 }
 
@@ -170,20 +191,20 @@ IMGUI_API void ImGui::LoadStyleFrom(const char* fileName) {
 	// Booleans
 	const char* AntiAliasedLines_Str = ini_get(ini_style, "ImGuiStyles", "AntiAliasedLines");
 	if (AntiAliasedLines_Str != NULL) {
-		if (strncmp(AntiAliasedLines_Str, "true", 4) == 0) style.AntiAliasedLines = true;
-		else if (strncmp(AntiAliasedLines_Str, "false", 5) == 0) style.AntiAliasedLines = false;
+		if (strncmpci(AntiAliasedLines_Str, "true", 4) == 0) style.AntiAliasedLines = true;
+		else if (strncmpci(AntiAliasedLines_Str, "false", 5) == 0) style.AntiAliasedLines = false;
 	}
 
 	const char* AntiAliasedLinesUseTex_Str = ini_get(ini_style, "ImGuiStyles", "AntiAliasedLinesUseTex");
 	if (AntiAliasedLinesUseTex_Str != NULL) {
-		if (strncmp(AntiAliasedLinesUseTex_Str, "true", 4) == 0) style.AntiAliasedLinesUseTex = true;
-		else if (strncmp(AntiAliasedLinesUseTex_Str, "false", 5) == 0) style.AntiAliasedLinesUseTex = false;
+		if (strncmpci(AntiAliasedLinesUseTex_Str, "true", 4) == 0) style.AntiAliasedLinesUseTex = true;
+		else if (strncmpci(AntiAliasedLinesUseTex_Str, "false", 5) == 0) style.AntiAliasedLinesUseTex = false;
 	}
 
 	const char* AntiAliasedFill_Str = ini_get(ini_style, "ImGuiStyles", "AntiAliasedFill");
 	if (AntiAliasedFill_Str != NULL) {
-		if (strncmp(AntiAliasedFill_Str, "true", 4) == 0) style.AntiAliasedFill = true;
-		else if (strncmp(AntiAliasedFill_Str, "false", 5) == 0) style.AntiAliasedFill = false;
+		if (strncmpci(AntiAliasedFill_Str, "true", 4) == 0) style.AntiAliasedFill = true;
+		else if (strncmpci(AntiAliasedFill_Str, "false", 5) == 0) style.AntiAliasedFill = false;
 	}
 
 	#define MAX_BEFORE_COMMA_SIZE 512
@@ -240,5 +261,306 @@ IMGUI_API void ImGui::LoadStyleFrom(const char* fileName) {
 
 	ini_free(ini_style);
 	ini_style = NULL;
+}
+
+// All The Code From Here Is Modified Version Of https://github.com/rxi/ini
+// It's Packed Into 1 File For "Ease" Of Use
+
+/**
+ * Copyright (c) 2016 rxi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+struct ini_t {
+  char *data;
+  char *end;
+};
+
+/* Case insensitive string compare */
+static int strcmpci(const char *a, const char *b) {
+  for (;;) {
+	int d = tolower(*a) - tolower(*b);
+	if (d != 0 || !*a) {
+	  return d;
+	}
+	a++, b++;
+  }
+}
+
+/* Returns the next string in the split data */
+static char* next(ini_t *ini, char *p) {
+  p += strlen(p);
+  while (p < ini->end && *p == '\0') {
+	p++;
+  }
+  return p;
+}
+
+static void trim_back(ini_t *ini, char *p) {
+  while (p >= ini->data && (*p == ' ' || *p == '\t' || *p == '\r')) {
+	*p-- = '\0';
+  }
+}
+
+static char* discard_line(ini_t *ini, char *p) {
+  while (p < ini->end && *p != '\n') {
+	*p++ = '\0';
+  }
+  return p;
+}
+
+
+static char *unescape_quoted_value(ini_t *ini, char *p) {
+  /* Use `q` as write-head and `p` as read-head, `p` is always ahead of `q`
+   * as escape sequences are always larger than their resultant data */
+  char *q = p;
+  p++;
+  while (p < ini->end && *p != '"' && *p != '\r' && *p != '\n') {
+	if (*p == '\\') {
+	  /* Handle escaped char */
+	  p++;
+	  switch (*p) {
+		default   : *q = *p;    break;
+		case 'r'  : *q = '\r';  break;
+		case 'n'  : *q = '\n';  break;
+		case 't'  : *q = '\t';  break;
+		case '\r' :
+		case '\n' :
+		case '\0' : goto end;
+	  }
+
+	} else {
+	  /* Handle normal char */
+	  *q = *p;
+	}
+	q++, p++;
+  }
+end:
+  return q;
+}
+
+
+/* Splits data in place into strings containing section-headers, keys and
+ * values using one or more '\0' as a delimiter. Unescapes quoted values */
+static void split_data(ini_t *ini) {
+  char *value_start, *line_start;
+  char *p = ini->data;
+
+  while (p < ini->end) {
+	switch (*p) {
+	  case '\r':
+	  case '\n':
+	  case '\t':
+	  case ' ':
+		*p = '\0';
+		/* Fall through */
+
+	  case '\0':
+		p++;
+		break;
+
+	  case '[':
+		p += strcspn(p, "]\n");
+		*p = '\0';
+		break;
+
+	  case ';':
+		p = discard_line(ini, p);
+		break;
+
+	  default:
+		line_start = p;
+		p += strcspn(p, "=\n");
+
+		/* Is line missing a '='? */
+		if (*p != '=') {
+		  p = discard_line(ini, line_start);
+		  break;
+		}
+		trim_back(ini, p - 1);
+
+		/* Replace '=' and whitespace after it with '\0' */
+		do {
+		  *p++ = '\0';
+		} while (*p == ' ' || *p == '\r' || *p == '\t');
+
+		/* Is a value after '=' missing? */
+		if (*p == '\n' || *p == '\0') {
+		  p = discard_line(ini, line_start);
+		  break;
+		}
+
+		if (*p == '"') {
+		  /* Handle quoted string value */
+		  value_start = p;
+		  p = unescape_quoted_value(ini, p);
+
+		  /* Was the string empty? */
+		  if (p == value_start) {
+			p = discard_line(ini, line_start);
+			break;
+		  }
+
+		  /* Discard the rest of the line after the string value */
+		  p = discard_line(ini, p);
+
+		} else {
+		  /* Handle normal value */
+		  p += strcspn(p, "\n");
+		  trim_back(ini, p - 1);
+		}
+		break;
+	}
+  }
+}
+
+
+ini_t* ini_load_txt(const char *iniTxt) {
+  ini_t *ini = NULL;
+  int len = 0;
+
+  if (iniTxt == NULL) goto fail;
+  len = strlen(iniTxt);
+
+  /* Init ini struct */
+  ini = (ini_t*)malloc(sizeof(ini_t));
+  if (!ini) goto fail;
+  memset(ini, 0, sizeof(*ini));
+
+  /* Load file content into memory, null terminate, init end var */
+  ini->data = (char*)malloc(len + 1);
+  ini->data[len] = '\0';
+  ini->end = ini->data + len;
+  strncpy(ini->data, iniTxt, len);
+
+  /* Prepare data */
+  split_data(ini);
+
+  return ini;
+
+fail:
+  if (ini) ini_free(ini);
+  return NULL;
+}
+
+ini_t* ini_load(const char *filename) {
+  ini_t *ini = NULL;
+  FILE *fp = NULL;
+  int n, sz;
+
+  /* Init ini struct */
+  ini = (ini_t*)malloc(sizeof(ini_t));
+  if (!ini) {
+	goto fail;
+  }
+  memset(ini, 0, sizeof(ini_t));
+
+  /* Open file */
+  fp = fopen(filename, "rb");
+  if (!fp) {
+	goto fail;
+  }
+
+  /* Get file size */
+  fseek(fp, 0, SEEK_END);
+  sz = ftell(fp);
+  rewind(fp);
+
+  /* Load file content into memory, null terminate, init end var */
+  ini->data = (char*)malloc(sz + 1);
+  ini->data[sz] = '\0';
+  ini->end = ini->data  + sz;
+  n = fread(ini->data, 1, sz, fp);
+  if (n != sz) {
+	goto fail;
+  }
+
+  /* Prepare data */
+  split_data(ini);
+
+  /* Clean up and return */
+  fclose(fp);
+  return ini;
+
+fail:
+  if (fp) fclose(fp);
+  if (ini) ini_free(ini);
+  return NULL;
+}
+
+
+void ini_free(ini_t *ini) {
+  free(ini->data);
+  free(ini);
+}
+
+
+const char* ini_get(ini_t *ini, const char *section, const char *key) {
+  if (ini == NULL) {
+	return NULL;
+  }
+
+  char *current_section = NULL;
+  char *val;
+  char *p = ini->data;
+
+  if (*p == '\0') {
+	p = next(ini, p);
+  }
+
+  while (p < ini->end) {
+	if (*p == '[') {
+	  /* Handle section */
+	  current_section = p + 1;
+
+	} else {
+	  /* Handle key */
+	  val = next(ini, p);
+	  if (!section || !strcmpci(section, current_section)) {
+		if (!strcmpci(p, key)) {
+		  return val;
+		}
+	  }
+	  p = val;
+	}
+
+	p = next(ini, p);
+  }
+
+  return NULL;
+}
+
+
+int ini_sget(
+  ini_t *ini, const char *section, const char *key,
+  const char *scanfmt, void *dst
+) {
+  const char *val = ini_get(ini, section, key);
+  if (!val) {
+	return 0;
+  }
+  if (scanfmt) {
+	sscanf(val, scanfmt, dst);
+  } else {
+	*((const char**) dst) = val;
+  }
+  return 1;
 }
 
